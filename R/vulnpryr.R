213916c53f48646c2a7bb75da81c6d2fb9bd8f9f
davidski <davidski@deadheaven.com>
2015-03-23 13:57:20 -0700

    Lint cleanup

--- R/vulnpryr.R
+++ R/vulnpryr.R
@@ -1,10 +1,7 @@
-#' vulnpryr: A package for computating the notorious bar statistic.
+#' vulnpryr: A package for prying additional utility into your CVSS scores.
 #'
-#' The foo package provides three categories of important functions:
-#' foo, bar and baz.
-#' 
 #' @section vulnpryr functions:
-#' The foo functions ...
+#' The vulnpryer functions ...
 #'
 #' @docType package
 #' @name vulnpryr
@@ -23,43 +20,52 @@ NULL
 #' @param impact_factor Amount to adjust if impact is not confidentiality
 #' @return Dataframe with the adjusted vuln
 vulnpryr <- function(cve_id, cvss_base, vulndb, avg_cvss_score = 6.2, 
-                     msp_factor = 2.5, edb_factor = 1.5, 
+                     msp_factor = 2.5, edb_factor = 1.5,
                      private_exploit_factor = .5, network_vector_factor = 2, 
                      impact_factor = 3) {
-  
+
   #cat(paste0("cve_id is ", cve_id))
   if (!any(vulndb$CVE_ID == cve_id)){
     #warning(paste0("Could not find ", cve_id))
-    return(data.frame(modified = FALSE, cvss = cvss_base))}
-  
+    return(data.frame(modified = FALSE, cvss = cvss_base))
+  }
+
   modified_score <- cvss_base
   modified_score <- (modified_score - avg_cvss_score) / avg_cvss_score * 10
   if (sum(vulndb[vulndb$CVE_ID == cve_id, "msp"]) >= 1) {
-    modified_score  <-  modified_score + msp_factor}
-  
+    modified_score <- modified_score + msp_factor
+  }
+
   # adjust up if exploit DB entry exists
   if (sum(vulndb[vulndb$CVE_ID == cve_id, "edb"]) >= 1) {
-    modified_score  <-  modified_score + edb_factor}
-  
+    modified_score <- modified_score + edb_factor
+  }
+
   # adjust up if a private exploit is known
   if (sum(vulndb[vulndb$CVE_ID == cve_id, "private_exploit"]) >= 1) {
-    modified_score  <-  modified_score + private_exploit_factor
+    modified_score <- modified_score + private_exploit_factor
   } else {
-    modified_score  <-  modified_score - private_exploit_factor
+    modified_score <- modified_score - private_exploit_factor
   }
-  
+
   # adjust down for impacts that aren't relevant to our loss scenario
-  if ((sum(vulndb[vulndb$CVE_ID == cve_id, "impact_integrity"] )< 1) &&
+  if ((sum(vulndb[vulndb$CVE_ID == cve_id, "impact_integrity"] ) < 1) &&
         (sum(vulndb[vulndb$CVE_ID == cve_id, "impact_confidentiality"]) < 1)) {
-    modified_score  <-  modified_score - impact_factor}
-  
+    modified_score  <-  modified_score - impact_factor
+  }
+
   # adjust down for attack vectors that aren't in our loss scenario
-  if (sum(vulndb[vulndb$CVE_ID==cve_id, "network_vector"]) < 1) {
-    modified_score  <-  modified_score - network_vector_factor }
-  
+  if (sum(vulndb[vulndb$CVE_ID == cve_id, "network_vector"]) < 1) {
+    modified_score  <-  modified_score - network_vector_factor
+  }
+
   # confirm that our modified score is within max/min limits
-  if (modified_score > 10) { modified_score <- 10 }
-  if (modified_score < 0)  { modified_score <-  0 }
-  
+  if (modified_score > 10) {
+    modified_score <- 10
+  }
+  if (modified_score < 0)  {
+    modified_score <-  0
+  }
+
   return(data.frame(modified = TRUE, cvss = modified_score))
 }
